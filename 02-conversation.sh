2b08659713497691be7bc9f52ab572da152bd1c1
lll <lll@gmail.com>
2025-12-15 17:43:31 +0530

    persons

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 echo "Ramesh:: Hi Suresh ,how are you"
 echo "Suresh:: i am fine how are you"
 echo "Ramesh:: what are you learning."
---
+++ 04-variable.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo "$person1:: welcome alaska $person2"
+echo "$person2:: thankyou $person1"
+echo "$person1:: do we have a deal"
+echo "$person2:: yes"
