9b60a72b0294b4df31123b44babdc0f313b0b4e3
lll <lll@gmail.com>
2025-12-15 17:52:09 +0530

    persons

--- 05-variable.sh
+++ 05-variable.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo " please enter your PIN number"
-read  PIN_NUMBER # here PIN_NUMBER is variable
+read -s  PIN_NUMBER # here PIN_NUMBER is variable
 echo "PIN enterd is:$PIN_NUMBER"
