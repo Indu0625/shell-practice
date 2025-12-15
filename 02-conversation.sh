734b9d98dfd8dd14a9b4fc462eae1067bc57a8e9
lll <lll@gmail.com>
2025-12-15 17:30:46 +0530

    some messages

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 echo "Ramesh:: Hi Suresh ,how are you"
 echo "Suresh:: i am fine how are you"
 echo "Ramesh:: what are you learning."
---
+++ 03-variable.sh
@@ -0,0 +1,8 @@
+
+#!/bin/bash
+person1=jhon
+person2=raheem
+
+echo "$person1:: Hi $person2 ,how are you"
+echo "$person2:: i am fine how are you"
+echo "$person1:: what are you learning."
