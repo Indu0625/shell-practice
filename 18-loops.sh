2fbfb28e62f4febf723322ed2dc4ecc54c1f11a1
lll <lll@gmail.com>
2025-12-17 15:42:20 +0530

    abc

---
+++ 18-loops.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+ 
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+ mkdir -p $LOGS_FOLDER
+echo "script started executed at: $(date)"|tee -a $LOG_FILE
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: please run this script with root previliges"
+    exit 1
+    fi
+    VALIDATE(){ # functions receive inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then
+    echo -e "ERROR:: Installing $2 .....$R failed $N"|tee -a $LOG_FILE
+    exit 1
+    else
+    echo -e  "installing $2 ....$G success $N"|tee -a $LOG_FILE
+    fi
+    }
+
+    for package in $@
+    do
+    echo "package is: $package
+    done
+    
+    
